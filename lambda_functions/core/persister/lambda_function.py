87983fe02ec36c84c4a8f46d84b9e6aaace7f765
Klaus Kaserer (windows) <android.kaserer@gmail.com>
2025-11-10 13:13:26 +0100

    init values

--- lambda_functions/core/persister/lambda_function.py
+++ lambda_functions/core/persister/lambda_function.py
@@ -1,6 +1,7 @@
 import os
 import json
 import boto3
+from decimal import Decimal
 
 
 DIGITAL_TWIN_INFO = json.loads(os.environ.get("DIGITAL_TWIN_INFO", None))
@@ -13,14 +14,24 @@ twinmaker_client = boto3.client("iottwinmaker")
 lambda_client = boto3.client("lambda")
 
 
+def floats_to_decimals(obj):
+    if isinstance(obj, list):
+        return [floats_to_decimals(x) for x in obj]
+    elif isinstance(obj, dict):
+        return {k: floats_to_decimals(v) for k, v in obj.items()}
+    elif isinstance(obj, float):
+        return Decimal(str(obj))
+    else:
+        return obj
+
 def lambda_handler(event, context):
     print("Hello from Persister!")
     print("Event: " + json.dumps(event))
 
     item = event.copy()
     item["id"] = str(item.pop("time"))
+    item = floats_to_decimals(item)
 
     dynamodb_table.put_item(Item=item)
 
     lambda_client.invoke(FunctionName=EVENT_CHECKER_LAMBDA_NAME, InvocationType="Event", Payload=json.dumps(event).encode("utf-8"))
-    
